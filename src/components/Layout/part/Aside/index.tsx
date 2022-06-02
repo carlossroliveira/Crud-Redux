fe4f77dff30023bcb80b73a511564830df4b2c87
Carlos Oliveira <carlos.sroliveira@hotmail.com>
2022-06-02 14:05:09 -0300

    Implementação de fonte

--- src/components/Layout/part/Aside/index.tsx
+++ src/components/Layout/part/Aside/index.tsx
@@ -2,5 +2,17 @@ import React from 'react';
 import styles from './aside.module.scss';
 
 export const Aside = () => {
-  return <div className={styles.container}>Aside</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.container__title}>
+        <span className={styles.container__title__span}>C</span>rud -{' '}
+        <span className={styles.container__title__span}>R</span>edux
+      </h1>
+
+      <div>
+        <a href="">Dash</a> <br />
+        <a href="">editar</a>
+      </div>
+    </div>
+  );
 };
--- src/components/Layout/part/StyleOne/index.tsx
+++ src/components/Layout/part/StyleOne/index.tsx
@@ -2,5 +2,10 @@ import React from 'react';
 import styles from './styleOne.module.scss';
 
 export const StyleOne = () => {
-  return <div className={styles.container}></div>;
+  const currentYear: number = new Date().getFullYear();
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.container__title}>&copy; {currentYear} | Carlos Oliveira</h1>
+    </div>
+  );
 };
